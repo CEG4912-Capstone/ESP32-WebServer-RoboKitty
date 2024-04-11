e93dc0155a6d8a5dd49934b29cd1dabad1a48e28
Kavin Gnanapandithan <kavin.balag@gmail.com>
2024-04-11 11:42:39 -0400

    Added code to control L298N motor driver.

--- roboKitty.ino
+++ roboKitty.ino
@@ -2,15 +2,59 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 
+// IR Sensor
+bool estop = false;
+int irSensor = 16;
+int irSensor1 = 17;
+
+
 // add your network settings
-const char* ssid = "WIFI_NAME";
-const char* password = "WIFI_PASSWORD";
+const char* ssid = "Kavin";
+const char* password = "12345678";
 
 WebServer server(80);
 
 const int ledPin26 = 26;
 const int ledPin27 = 27;
 
+bool backwardState = false;
+
+
+// Motor variables
+// Motor A
+
+int motor1Pin1 = 27;
+
+int motor1Pin2 = 26;
+
+int enable1Pin = 14;
+
+int motor2Pin1 = 25;
+
+int motor2Pin2 = 33;
+
+int enable2Pin = 15;
+
+// Setting PWM properties
+
+const int freq = 50000;
+
+const int pwmChannel = 0;
+const int pwmChannel1 = 1;
+
+const int resolution = 8;
+
+int dutyCycle = 190;
+
+void IRAM_ATTR ISR() {
+  Serial.println("Interrupt: Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+  estop = true;
+}
 
 // handle preflight request options
 void handleOptions() {
@@ -28,13 +72,13 @@ void handlePost() {
     server.send(200, "application/json", "{\"error\":\"Body not received\"}");
     return;
   }
-  
+
   String message = server.arg("plain"); // get the body
   Serial.print("POST Body: ");
   Serial.println(message);
 
   // get json response
-  DynamicJsonDocument doc(1024 * 4); 
+  DynamicJsonDocument doc(1024 * 4);
   DeserializationError error = deserializeJson(doc, message);
 
   if (error) {
@@ -45,50 +89,65 @@ void handlePost() {
   }
 
   //list of commands
+  const char* test = doc["movementType"];
+  Serial.println(test);
   JsonArray commands = doc.as<JsonArray>();
 
   for (JsonObject command : commands) {
+    detectIR();
+
+    if (estop)
+      break;
     // important command data
-    int steps = command["steps"];
+    int steps = (int) command["steps"];
     const char* movementType = command["movementType"]; // 'Move' | 'Turn'
-    const char* direction = command["direction"]; // 'Forward' | 'Backward' | 'Right' | 'Left'
+    const char* directionType = command["direction"]; // 'Forward' | 'Backward' | 'Right' | 'Left'
 
     // gotta process command after command to preserve the sequence
-    if(movementType == "Move"){
-      if(direction == "Forward"){
+    if (strcmp(movementType, "Move") == 0) {
+      if (strcmp(directionType, "Forward") == 0) {
 
         // robot.forward(steps)
-        Serial.println("Forward: "+steps);
+        Serial.println(steps);
+        forward(steps);
 
-      } else if(direction == "Backward"){
+
+      } else if (strcmp(directionType, "Backward") == 0) {
 
         // robot.backward(steps)
-        Serial.println("Backward: "+steps);
+        backward(steps);
+        //Serial.println("Backward: "+steps);
 
       }
-  
-    } else if(movementType == "Turn"){
-      if(direction == "Right"){
+
+    } else if (strcmp(movementType, "Turn") == 0) {
+      if (strcmp(directionType, "Right") == 0) {
 
         // robot.right(steps)
-        Serial.println("Turn right: "+steps);
+        right(steps);
+        //Serial.println("Turn right: "+steps);
 
-      } else if(direction == "Left"){
+      } else if (strcmp(directionType, "Left") == 0) {
 
         // robot.left(steps)
-        Serial.println("Turn left: "+steps);
-        
+        left(steps);
+        Serial.println("Turn left: " + steps);
+
       }
     }
-  
-  server.send(200, "application/json", "{\"message\":\"request received\"}");
+
   }
+  server.send(200, "application/json", "{\"message\":\"request received\"}");
+
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin26, OUTPUT);
   pinMode(ledPin27, OUTPUT);
+
+  pinMode(irSensor, INPUT_PULLUP);
+  pinMode(irSensor1, INPUT_PULLUP);
 
   WiFi.begin(ssid, password);
 
@@ -118,13 +177,146 @@ void setup() {
   server.on("/post", HTTP_POST, handlePost);
   server.on("/post", HTTP_OPTIONS, handleOptions); // Handle preflight requests
 
+
+  attachInterrupt(irSensor, ISR, RISING);
+  attachInterrupt(irSensor1, ISR, RISING);
+
   // start
   server.begin();
+
+  // sets the pins as outputs:
+
+  pinMode(motor1Pin1, OUTPUT);
+
+  pinMode(motor1Pin2, OUTPUT);
+
+  pinMode(enable1Pin, OUTPUT);
+
+  pinMode(motor2Pin1, OUTPUT);
+
+  pinMode(motor2Pin2, OUTPUT);
+
+  pinMode(enable2Pin, OUTPUT);
+
+  // configure LED PWM functionalitites
+
+  ledcSetup(pwmChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+
+  ledcAttachPin(enable1Pin, pwmChannel);
+
+  ledcAttachPin(enable2Pin, pwmChannel1);
+
+  Serial.begin(115200);
+
+  // testing
+
+  Serial.print("Testing DC Motor...");
 }
 
 void loop() {
-  server.handleClient();
+
+
+
+  if (!estop) {
+    server.handleClient();
+
+    if (backwardState)  {
+      ledcWrite(pwmChannel, 235); // right
+      ledcWrite(pwmChannel1, 245); // left
+    } else {
+      ledcWrite(pwmChannel, 230);
+      ledcWrite(pwmChannel1, 250);
+    }
+  } else {
+    stopMotor();
+  }
+
+
 }
+
+void detectIR() {
+  int state  = digitalRead(irSensor);
+  //Serial.println(state);
+
+  if (state == 1) {
+    estop = true;
+    Serial.println("Black detected");
+  }
+
+  if (estop) {
+    stopMotor();
+  }
+}
+
+void forward(int steps) {
+
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+  delay(steps * 500);
+
+  stopMotor();
+}
+
+void stopMotor() {
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+}
+
+
+
+
+void backward(int steps) {
+  backwardState = true;
+  Serial.println("Moving Backwards");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  delay(steps * 500);
+
+  stopMotor();
+  backwardState = false;
+
+}
+
+void right(int steps) {
+  Serial.println("Moving Left");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+
+  delay(steps / 90 * 1000);
+
+  stopMotor();
+}
+
+void left(int steps) {
+  Serial.println("Moving Right");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+
+  delay(steps / 90 * 1000);
+
+  stopMotor();
+}
+
+
 
 String getHtml() {
   //default web page
